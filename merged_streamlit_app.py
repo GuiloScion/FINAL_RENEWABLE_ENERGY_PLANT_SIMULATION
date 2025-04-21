7c86ce9a546705729e6f06d4baaae7b46783bf54
GuiloScion <161479672+GuiloScion@users.noreply.github.com>
2025-04-21 15:21:01 -0600

    Update merged_streamlit_app.py

--- merged_streamlit_app.py
+++ merged_streamlit_app.py
@@ -5,6 +5,7 @@ import time
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor  # Add this line
 import xgboost as xgb
 import mlflow
 import mlflow.sklearn
