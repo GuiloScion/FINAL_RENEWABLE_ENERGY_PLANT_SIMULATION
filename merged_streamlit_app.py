8c7bc3eb8a0f2ec3acae3b31eca3a38f5652ad1d
GuiloScion <161479672+GuiloScion@users.noreply.github.com>
2025-04-22 08:09:45 -0600

    Update merged_streamlit_app.py

--- merged_streamlit_app.py
+++ merged_streamlit_app.py
@@ -90,12 +90,12 @@ if st.sidebar.button("Train Model"):
      # Start time
 start_time = time.time()
  
-   if model_choice == "Random Forest":
-        model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-   elif model_choice == "Gradient Boosting":
-        model = GradientBoostingRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-   else:
-        model = XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+  if model_choice == "Random Forest":
+       model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+  elif model_choice == "Gradient Boosting":
+       model = GradientBoostingRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+  else:
+       model = XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
  
     model.fit(X_train, y_train)
     training_time = time.time() - start_time
